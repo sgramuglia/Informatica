8f19cf8064ab712da886af5b1d714c6238d04017
Sofía Gramuglia <38789070+sgramuglia@users.noreply.github.com>
2021-04-17 15:31:21 -0300

    Add files via upload

--- PRACTICA_2/ej12.py
+++ PRACTICA_2/ej12.py
@@ -1,16 +1,20 @@
 mrks = {}
 qnt = int(input("Cantidad de alumnos: "))
+mrklist = []
+nm = input("Nombre: ")
+mrk = int(input("Nota: "))
 for i in range(qnt):
-    nm = input("Nombre: ")
-    mrk = int(input("Nota: "))
-    while mrk >= 0:
-        mrk = int(input("Nota: "))
-        mrks[nm] = mrk
+    if nm not in mrks.keys() and mrk > 0:
+       while mrk > 0:
+            mrklist.append(mrk)
+            mrks[nm] = mrklist
+            mrk = int(input("Nota: "))
+    else:
+        print("Alumno ya cargado")
 for nm, mrk in mrks.items(): 
     marks = 0
     qn = 0
-    for ex in mrks.values():
+    for ex in mrklist:
         marks += ex
         qn += 1
     print("Promdedio de: ", nm, ":", marks/qn)
-    print(marks)
